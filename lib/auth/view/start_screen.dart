323e304067ac88440cad8f745453e3f30ddea941
jaehee21232 <a62533196@gmail.com>
2023-04-27 00:02:19 +0900

    feat: 로그인 기능 추가

--- lib/auth/view/start_screen.dart
+++ lib/auth/view/start_screen.dart
@@ -1,29 +1,53 @@
+import 'package:chatapp/auth/component/Dialog.dart';
+import 'package:chatapp/auth/riverpod/state_provider.dart';
+import 'package:chatapp/common/const/color.dart';
 import 'package:chatapp/common/const/data.dart';
 import 'package:chatapp/common/const/style/font_style.dart';
 import 'package:chatapp/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends ConsumerWidget {
   const StartScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(loginProvider);
     return DefaultLayout(
         body: Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          _LogoText(),
+          const _LogoText(),
+          const _LoginNumber(),
           SizedBox(
             width: double.infinity,
+            height: 50,
             child: ElevatedButton(
-              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: black),
+              onPressed: () {
+                if (state == 0) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => LoginDialog(),
+                  );
+                  ref.read(loginProvider.notifier).update((state) => state + 1);
+                } else if (state >= 30) {
+                  Navigator.pushNamed(context, "/registerscreen");
+                } else {
+                  ref.read(loginProvider.notifier).update((state) => state + 1);
+                }
+              },
               child: FutureBuilder(
                 future: checkLogin(),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
-                    return Text("");
+                    return const Text("");
                   } else {
-                    return Text(snapshot.data.toString());
+                    return Text(
+                      snapshot.data.toString(),
+                      style: styleButtonText,
+                    );
                   }
                 },
               ),
@@ -63,6 +87,19 @@ class _LogoText extends StatelessWidget {
           style: logoText,
         ),
       ],
+    );
+  }
+}
+
+class _LoginNumber extends ConsumerWidget {
+  const _LoginNumber({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(loginProvider);
+    return Text(
+      state.toString() == "0" ? "" : state.toString(),
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
     );
   }
 }
