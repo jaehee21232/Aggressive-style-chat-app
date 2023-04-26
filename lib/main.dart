e439329e0affac51923a2b86953cffb00714c932
jaehee21232 <a62533196@gmail.com>
2023-04-27 00:01:04 +0900

    feat: routes, ProviderScope add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:chatapp/auth/view/register_screen.dart';
 import 'package:chatapp/auth/view/start_screen.dart';
 import 'package:chatapp/common/const/style/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'chatapp',
-      theme: whiteTheme,
-      home: StartScreen(),
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'chatapp',
+        theme: whiteTheme,
+        home: StartScreen(),
+        routes: {
+          "/registerscreen": (context) => const RegisterScreen(),
+        },
+      ),
     );
   }
 }
